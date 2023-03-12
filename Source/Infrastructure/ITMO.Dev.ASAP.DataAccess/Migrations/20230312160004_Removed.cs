a119654e2569837877d9815601cd6ab39725c104
ronimizy <ronimizy@outlook.com>
2023-03-12 19:03:55 +0300

    refactor: removed GithubSubmission

---
+++ Source/Infrastructure/ITMO.Dev.ASAP.DataAccess/Migrations/20230312160004_Removed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITMO.Dev.ASAP.DataAccess.Migrations
+{
+    public partial class Removed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Submissions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Submissions",
+                type: "text",
+                nullable: false,
+                defaultValue: string.Empty);
+        }
+    }
+}
