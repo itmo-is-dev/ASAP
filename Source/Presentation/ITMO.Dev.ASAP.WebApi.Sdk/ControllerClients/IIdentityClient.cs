993a5857fc2bd86a44f4e811656aea8712576e71
Эмин <eminbegin@gmail.com>
2023-03-25 18:29:18 +0300

    fix: reformat code

--- Source/Presentation/ITMO.Dev.ASAP.WebApi.Sdk/ControllerClients/IIdentityClient.cs
+++ Source/Presentation/ITMO.Dev.ASAP.WebApi.Sdk/ControllerClients/IIdentityClient.cs
@@ -10,6 +10,7 @@ public interface IIdentityClient
     Task ChangeUserRoleAsync(string username, string roleName, CancellationToken cancellationToken = default);
 
     Task CreateUserAccountAsync(Guid id, CreateUserAccountRequest request, CancellationToken cancellationToken = default);
+
     Task UpdatePasswordAsync(UpdatePasswordRequest request, CancellationToken cancellationToken = default);
     Task UpdateUsernameAsync(UpdateUsernameRequest request, CancellationToken cancellationToken = default);
     Task<PasswordOptionsDto> GetPasswordOptionsAsync(CancellationToken cancellationToken = default);
