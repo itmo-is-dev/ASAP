2f0f89a67189722ac47d231ebb018cf0b91674c5
Max <for_maxim@bk.ru>
2023-04-24 21:49:03 +0300

    chore: add migration

---
+++ src/core/Infrastructure/ITMO.Dev.ASAP.DataAccess/Migrations/20230424164045_RenamedDeadlinePolicyToDeadlinePenalty.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ITMO.Dev.ASAP.DataAccess.Migrations
+{
+    public partial class RenamedDeadlinePolicyToDeadlinePenalty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "DeadlinePolicy",
+                newName: "DeadlinePenalty");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "DeadlinePenalty",
+                newName: "DeadlinePolicy");
+        }
+    }
+}
