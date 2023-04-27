c28109d7e6585731eb65a78edb1bb23b47e317d6
Max <for_maxim@bk.ru>
2023-04-27 14:16:55 +0300

    test: add test for deadline policy

---
+++ tests/ITMO.Dev.ASAP.Tests/Deadlines/DeadlinePolicyTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using ITMO.Dev.ASAP.Core.Deadlines.DeadlinePenalties;
+using ITMO.Dev.ASAP.Core.Deadlines.DeadlinePolicies;
+using ITMO.Dev.ASAP.Core.Study;
+using ITMO.Dev.ASAP.Core.ValueObject;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace ITMO.Dev.ASAP.Tests.Deadlines;
+
+public class DeadlinePolicyTest : TestBase
+{
+    [Fact]
+    public async Task GetDeadlinePenaltyFromSubject()
+    {
+        SubjectCourse subjectCourse = await Context.SubjectCourses.FirstAsync();
+
+        subjectCourse.DeadlinePolicies.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public async Task GetDeadlinePenaltyFromPolicy()
+    {
+        FractionDeadlinePenalty fractionPenalty = new(TimeSpan.FromDays(7), Fraction.FromDenormalizedValue(20));
+        AbsoluteDeadlinePenalty absolutePenalty = new(TimeSpan.FromHours(3), new Points(1));
+
+        DeadlinePolicy policy = new(Guid.NewGuid());
+        policy.AddDeadlinePenalty(fractionPenalty);
+        policy.AddDeadlinePenalty(absolutePenalty);
+        Context.DeadlinePolicies.Add(policy);
+
+        policy.DeadlinePenalties.Should().NotBeEmpty();
+        await Context.SaveChangesAsync();
+    }
+}
