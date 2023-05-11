5e5cf31bf014270f14fd811d745ac1fc8b0bfbf4
Bl4ckburn <5lickit95loth@gmail.com>
2023-05-11 13:01:19 +0500

    fix: AssignmentCreatedHandler addition

---
+++ src/github/Application/ITMO.Dev.ASAP.Github.Application.Handlers/Assignments/AssignmentCreatedHandler.cs
@@ -0,0 +1,30 @@
+﻿using ITMO.Dev.ASAP.Application.Contracts.Study.Assignments.Notifications;
+using ITMO.Dev.ASAP.Github.Application.DataAccess;
+using ITMO.Dev.ASAP.Github.Common.Exceptions.Entities;
+using ITMO.Dev.ASAP.Github.Domain.Assignments;
+using MediatR;
+
+namespace ITMO.Dev.ASAP.Github.Application.Handlers.Assignments;
+
+public class AssignmentCreatedHandler : INotificationHandler<AssignmentCreated.Notification>
+{
+    private readonly IDatabaseContext _context;
+
+    public AssignmentCreatedHandler(IDatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Handle(AssignmentCreated.Notification notification, CancellationToken cancellationToken)
+    {
+        if (await _context.Assignments.FindAsync(notification.Assignment.Id) is not null)
+        {
+            throw GithubAssignmentException.AssignmentAlreadyExists(notification.Assignment.Id, notification.Assignment.Title);
+        }
+
+        var assignment = new GithubAssignment(notification.Assignment.Id, notification.Assignment.ShortName);
+
+        await _context.Assignments.AddAsync(assignment, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ src/github/ITMO.Dev.ASAP.Github.Common/Exceptions/GithubAssignmentException.cs
@@ -0,0 +1,9 @@
+﻿namespace ITMO.Dev.ASAP.Github.Common.Exceptions.Entities;
+
+public class GithubAssignmentException : AsapGithubException
+{
+    private GithubAssignmentException(string? message) : base(message) { }
+
+    public static GithubAssignmentException AssignmentAlreadyExists(Guid assignmentId, string assignmentName)
+        => new GithubAssignmentException($"Github assignment {assignmentName} with id: {assignmentId} already exists");
+}
