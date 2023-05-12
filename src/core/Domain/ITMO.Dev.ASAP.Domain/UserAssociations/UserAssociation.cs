a446f2c3397c548da8cb0cce335be25e2ee26567
Max <for_maxim@bk.ru>
2023-05-12 15:22:12 +0300

    refactor: remove comment

--- src/core/Domain/ITMO.Dev.ASAP.Domain/UserAssociations/UserAssociation.cs
+++ src/core/Domain/ITMO.Dev.ASAP.Domain/UserAssociations/UserAssociation.cs
@@ -8,8 +8,6 @@ public abstract partial class UserAssociation : IEntity<Guid>
     protected UserAssociation(Guid id, User user) : this(id)
     {
         User = user;
-
-        // user.AddAssociation(this);
     }
 
     public virtual User User { get; protected init; }
