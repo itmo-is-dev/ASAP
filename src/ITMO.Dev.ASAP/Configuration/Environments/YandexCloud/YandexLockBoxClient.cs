851c2cd691556ce98fd32598f94719898bbc8140
Roman Makarevich <remmybump@gmail.com>
2023-05-20 22:10:07 +0300

    Implemented reading configuration from LockBox

---
+++ src/ITMO.Dev.ASAP/Configuration/Environments/YandexCloud/YandexLockBoxClient.cs
@@ -0,0 +1,76 @@
+﻿using ITMO.Dev.ASAP.Configuration;
+using ITMO.Dev.ASAP.Exceptions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace ITMO.Dev.ASAP;
+
+internal class YandexLockBoxClient
+{
+    private readonly string _token;
+
+    public YandexLockBoxClient(string token)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(token, nameof(token));
+
+        _token = token;
+    }
+
+    internal async Task<LockBoxEntry[]> GetEntries(string secretId)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(secretId, nameof(secretId));
+
+        const string baseUrl = "https://payload.lockbox.api.cloud.yandex.net/";
+        string requestUri = $"/lockbox/v1/secrets/{secretId}/payload";
+
+        using var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri(baseUrl),
+            DefaultRequestHeaders =
+            {
+                Authorization = new AuthenticationHeaderValue("Bearer", _token),
+            },
+        };
+
+        using var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+        HttpResponseMessage resp = await httpClient.SendAsync(request);
+
+        if (resp.StatusCode is not HttpStatusCode.OK)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Unable to receive secrets from Yandex LockBox.");
+            stringBuilder.AppendLine($"HTTP Status code: {resp.StatusCode:D)}");
+            string body = await resp.Content.ReadAsStringAsync();
+
+            if (!string.IsNullOrWhiteSpace(body))
+            {
+                stringBuilder.AppendLine("HTTP Response:");
+                stringBuilder.AppendLine(body);
+            }
+
+            throw new StartupException(stringBuilder.ToString());
+        }
+
+        string respBody = await resp.Content.ReadAsStringAsync();
+        try
+        {
+            LockBoxEntry[]? entries = JsonConvert.DeserializeObject<JObject>(respBody)?
+                .GetValue("entries", StringComparison.Ordinal)?
+                .ToObject<LockBoxEntry[]>();
+
+            if (entries?.Any() is not true)
+            {
+                throw new StartupException("Secrets cannot be null or empty");
+            }
+
+            return entries;
+        }
+        catch
+        {
+            throw new StartupException("Yandex LockBox have not returned any secrets");
+        }
+    }
+}
---
+++ src/ITMO.Dev.ASAP/Configuration/Environments/YandexCloud/YandexTokenProvider.cs
@@ -0,0 +1,67 @@
+﻿using ITMO.Dev.ASAP.Exceptions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Text;
+
+namespace ITMO.Dev.ASAP;
+
+internal static class YandexTokenProvider
+{
+    internal static async Task<string> GetToken()
+    {
+        const string baseUrl = "http://169.254.169.254/";
+        const string requestUri = "computeMetadata/v1/instance/service-accounts/default/token";
+
+        using var httpClient = new HttpClient { BaseAddress = new Uri(baseUrl) };
+
+        using var request = new HttpRequestMessage(HttpMethod.Get, requestUri)
+        {
+            Headers =
+            {
+                { "Metadata-Flavor", "Google" },
+            },
+        };
+
+        HttpResponseMessage resp = await httpClient.SendAsync(request);
+
+        if (resp.StatusCode is not HttpStatusCode.OK)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Unable to receive IAM service account token from Yandex Compute Cloud.");
+            stringBuilder.AppendLine($"HTTP Status code: {resp.StatusCode:D)}");
+            string body = await resp.Content.ReadAsStringAsync();
+
+            if (!string.IsNullOrWhiteSpace(body))
+            {
+                stringBuilder.AppendLine("HTTP Response:");
+                stringBuilder.AppendLine(body);
+            }
+
+            throw new StartupException(stringBuilder.ToString());
+        }
+
+        string content = await resp.Content.ReadAsStringAsync();
+        JObject? jsonContent = JsonConvert.DeserializeObject<JObject>(content);
+
+        if (jsonContent is null)
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Unable to parse IAM service account token from Yandex Compute Cloud.");
+            stringBuilder.AppendLine("Cannot parse JSON. Original response:");
+            stringBuilder.AppendLine(content);
+            throw new StartupException(stringBuilder.ToString());
+        }
+
+        if (!jsonContent.TryGetValue("access_token", StringComparison.Ordinal, out JToken? accessToken))
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Unable to parse IAM service account token from Yandex Compute Cloud.");
+            stringBuilder.AppendLine("Cannot find access token in original response:");
+            stringBuilder.AppendLine(content);
+            throw new StartupException(stringBuilder.ToString());
+        }
+
+        return accessToken.ToString();
+    }
+}
